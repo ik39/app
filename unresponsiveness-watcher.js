270c38d3b1d469651d31a5ef29e5bbc25cbc396d
Glitch (hello-express) <none>
2022-05-02 22:15:56 +0000

    😋🐆 Checkpoint ./unresponsiveness-watcher.js:165551/199 ./watch.json:165551/6 ./package.json:165551/16

--- unresponsiveness-watcher.js
+++ unresponsiveness-watcher.js
@@ -4,12 +4,13 @@
   
   let lastSeenTime = Date.now();
   while(1) {
-    await new Promise(r => setTimeout(r, 1000));
+    await new Promise(r => setTimeout(r, 5000));
     
     if(Date.now() - lastSeenTime > 1000*30) process.exit();
     
     try {
-      let text = await fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me/status`, {timeout:5000}).then(r => r.text());
+      // let text = await fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me/status`, {timeout:5000}).then(r => r.text());
+      let text = await fetch(`http://localhost:3000/status`, {timeout:5000}).then(r => r.text());
       if(text === "Online.") {
         lastSeenTime = Date.now();
         console.log("Seen!");
