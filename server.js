9263afc5d0a05b3550723c90b78a03a1edbf7a1e
Glitch (hello-express) <none>
2022-05-13 08:07:41 +0000

    💆🐈 Checkpoint ./README.md:165551/307 ./server.js:165551/980 ./watch.json:165551/1

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// To use this bot on your own server, you need to:
-//   1. Register a Discord application. Some helpful instructions are available in Step 2 of this blog post: https://web.archive.org/web/20220429161834/https://www.alpharithms.com/how-to-code-a-discord-bot-in-javascript-444917/
-//   2. Remix/fork this server (you should see a button in the UI here somewhere)
-//   3. Add a variable in the .env file called DISCORD_TOKEN and put your Discord bot's token as the value
-// See here for info on how to use this bot: https://discord.com/channels/970057744612724746/970057745665499148/970317365755654144
+// See the README.md file for instructions on how to install this bot on your own server.
 
+// Note that if you edit this file, then you need to make an edit to watch.json to trigger the server restart.
+// That's because I set `throttle` to a large number because I don't like it when Glitch 
 
 (async function() { 
   
